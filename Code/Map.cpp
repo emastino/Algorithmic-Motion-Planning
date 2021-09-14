1c3856a2c0b1f2bf1d58b80724fbe76909e66fbd
emastino <emastino@gmail.com>
2021-09-14 17:18:14 -0600

    New map class. New features include polygon and vertex structs, can read vertext text file to populate polygon vertices.

---
+++ Code/Map.cpp
@@ -0,0 +1,104 @@
+﻿// Discrete Map.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+#include "Map.h"
+
+using namespace std;
+
+int main()
+{
+
+	// map limits
+	std::tuple<float, float> xLim = std::make_tuple(0.0,13.2);
+	std::tuple<float, float> yLim = std::make_tuple(0.0,17.8);
+	// map start and goal
+	std::tuple<float, float> start = std::make_tuple(0.0,0.0);
+	std::tuple<float, float> goal = std::make_tuple(10.0,10.0);
+
+
+	// read points in from obstacles.txt and make a vector of obstacles
+	// This will be a vector of polygons, which in turn a vector of positions
+	std::vector<polygon> obstacles;
+
+	string line; // whole line of text file
+	int count = 1;
+	vertex vertex;
+
+	ifstream myfile("obstacles.txt");
+	while ( getline (myfile,line) ){
+
+		std::istringstream ss( line);
+
+		// create new polygon object to be added
+		polygon poly;
+
+		while (ss){
+      string s; // string seperated by comma
+
+      if (!getline( ss, s, ',' )) break;
+
+			if(count ==1){
+				// add x value of point
+				vertex.x = std::stof(s);
+				count = 2;
+			}
+			else{
+				// add y value of point
+				vertex.y = std::stof(s);
+				// add vertex to the polygon in obstacles
+				poly.vertices.push_back(vertex);
+				count =1;
+			}
+
+
+    }
+		obstacles.push_back(poly);
+	}
+	myfile.close();
+
+
+// // Debugging
+// // Display what is stored in obstacles
+// int polyCnt = 1;
+// for (const auto& it: obstacles){
+// 	cout << "Polygon " << polyCnt << endl;
+// 	for (const auto& it1: it.vertices){
+// 		cout << "pos = [" << it1.x << "," << it1.y << "]" << endl;
+// 	}
+// 	polyCnt++;
+// }
+
+// // Debugging
+// test out polygon struct funstionality
+// polygon triangle;
+// for(int i = 0; i <3; i++){
+// 	pos p;
+// 	p.x = 2.0+i;
+// 	p.y = i*5.0;
+// 	triangle.points.push_back(p);
+// }
+// cout << "Triangle Points" << endl;
+// for(const auto& it : triangle.points){
+// 	cout << it.x << ", " << it.y << endl;
+// }
+
+
+
+	Map mapa(xLim, yLim, start, goal);
+
+
+
+
+	return 0;
+}
---
+++ Code/Map.h
@@ -0,0 +1,73 @@
+﻿// Discrete Map.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+
+struct vertex{
+	float x,y;
+};
+
+// Polygon struct
+struct polygon{
+	std::vector<vertex> vertices;
+};
+
+
+
+class Map {
+	float xMapMin, xMapMax, yMapMin, yMapMax, sx, sy,gx,gy;
+	std::vector<polygon> obstacles;
+	public:
+	Map( std::tuple<float,float>, std::tuple<float,float>, std::tuple<float,float>, std::tuple<float,float>);
+	bool pointCollision(vertex); // find out if a point collides with any of the obstacles
+	bool polygonCollision(); // find out if a polygon collides with another polygon
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Map Constructor
+////////////////////////////////////////////////////////////////////////////////
+Map::Map( std::tuple<float,float> xLim,  std::tuple<float,float> yLim, std::tuple<float,float> start, std::tuple<float,float> goal){
+	std::tie(xMapMin, xMapMax) = xLim;
+	std::tie(yMapMin, yMapMax) = yLim;
+
+	std::tie(sx,sy) = start;
+	std::tie(gx,gy) = goal;
+
+	// Debugging
+	// cout << "xLim = [" << xMapMin << "," << xMapMax << "]" << endl;
+	// cout << "yLim = [" << yMapMin << "," << yMapMax << "]" << endl;
+	// cout << "start = (" << sx << "," << sy << ")" << endl;
+	// cout << "goal = (" << gx << "," << gy << ")" << endl;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// pointCollision
+////////////////////////////////////////////////////////////////////////////////
+
+bool Map::pointCollision(vertex point){
+
+	return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// polygonCollision
+////////////////////////////////////////////////////////////////////////////////
+
+bool Map::polygonCollision(){
+
+	return false;
+}
