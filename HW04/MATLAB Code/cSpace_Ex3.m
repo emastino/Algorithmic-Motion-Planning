db486a8f154199909e0d442a21298adccc35fd99
emastino <emastino@gmail.com>
2021-09-23 20:51:34 -0600

    All questions answered

--- HW04/MATLAB Code/cSpace_Ex3.m
+++ HW04/MATLAB Code/cSpace_Ex3.m
@@ -7,8 +7,6 @@ clc; close all; clear all;
 %% Exercise 3.a
 ospace= importdata("obstacle_Ex_3_a.txt");
 
-
-
 figure
 
 hold on
@@ -38,7 +36,7 @@ axis on
 xticks([1/n 0.25 0.5 0.75 1]*n)
 xticklabels({'0','90','180','270','360'})
 yticks([1/n 0.25 0.5 0.75 1]*n)
-yticklabels({'0','90','180','270','360'})
+yticklabels({'360','270','180','90','0'})
 
 xlabel("\theta_1 [deg]");ylabel("\theta_2 [deg]")
 
@@ -56,6 +54,83 @@ sgtitle('Exercise 3.a Workspace vs Configuration Space')
 % end
 
 
+%% Exercise 3.b
+clear all
 
+ospace= importdata("obstacle_Ex_3_b.txt");
 
+figure
+hold on
+subplot(1,2,1)
+for i = 1:size(ospace,1)
+   vertices = ospace(i,:);
+   pgon(i) = polyshape(vertices(1:2:end),vertices(2:2:end));
+end
+plot(pgon,'FaceColor','blue','FaceAlpha',0.1)
+axis equal
+xlim([-2 2]); ylim([-2 2]);
+xlabel('x axis'); ylabel('y-axis');
+grid minor
+
+hold off
+
+% import c-space
+cspace_a= importdata("cSpace_Ex_3_b.txt");
+n = sqrt(size(cspace_a,1));
+
+collisions = flip(reshape(cspace_a(:,3), [n,n]));
+subplot(1,2,2)
+
+imshow(collisions)
+grid minor
+axis on
+
+xticks([1/n 0.25 0.5 0.75 1]*n)
+xticklabels({'0','90','180','270','360'})
+yticks([1/n 0.25 0.5 0.75 1]*n)
+yticklabels({'360','270','180','90','0'})
+
+xlabel("\theta_1 [deg]");ylabel("\theta_2 [deg]")
+
+sgtitle('Exercise 3.b Workspace vs Configuration Space') 
+
+%% Exercise 3.c
+clear all
+
+ospace= importdata("obstacle_Ex_3_c.txt");
+
+figure
+hold on
+subplot(1,2,1)
+for i = 1:size(ospace,1)
+   vertices = ospace(i,:);
+   pgon(i) = polyshape(vertices(1:2:end),vertices(2:2:end));
+end
+plot(pgon,'FaceColor','blue','FaceAlpha',0.1)
+axis equal
+xlim([-2 2]); ylim([-2 2]);
+xlabel('x axis'); ylabel('y-axis');
+grid minor
+
+hold off
+
+% import c-space
+cspace_a= importdata("cSpace_Ex_3_c.txt");
+n = sqrt(size(cspace_a,1));
+
+collisions = flip(reshape(cspace_a(:,3), [n,n]));
+subplot(1,2,2)
+
+imshow(collisions)
+grid minor
+axis on
+
+xticks([1/n 0.25 0.5 0.75 1]*n)
+xticklabels({'0','90','180','270','360'})
+yticks([1/n 0.25 0.5 0.75 1]*n)
+yticklabels({'360','270','180','90','0'})
+
+xlabel("\theta_1 [deg]");ylabel("\theta_2 [deg]")
+
+sgtitle('Exercise 3.c Workspace vs Configuration Space') 
 
--- HW04/arm_cSpace.cpp
+++ HW04/arm_cSpace.cpp
@@ -189,13 +189,13 @@ int main()
 	// make a map object
 	map map_2(xLim, yLim, start, goal, obstacles_2);
 
-  double delta_theta = 0.5; // number of points from 0-360 degrees
+  double delta_theta = 2; // number of points from 0-360 degrees
 
   double arm_1_length = 1;
   double arm_2_length = 1;
 
-  double theta_1 = 0;
-  double theta_2 = 0;
+  double theta_1 = -720;
+  double theta_2 = -720;
 
   vertex origin, arm1,arm2;
   origin.x = 0; origin.y =0;
@@ -206,14 +206,14 @@ int main()
 
   ofstream cSpaceFile ("cSpace_Ex_3_b.txt");
 
-  while(theta_1 <= 360){
+  while(theta_1 <= 720){
 
-    while(theta_2 <= 360){
+    while(theta_2 <= 720){
 
       // make function to get position of points
       arm1.x = arm_1_length*cos(theta_1*PI/180.0);
       arm1.y = arm_1_length*sin(theta_1*PI/180.0);
-
+      // cout << "Arm 1 (x,y) = " << arm1.x << ", " << arm1.y << endl;
       // check collision of arm 1
       if(map_2.lineCollision(origin, arm1)){
         collisionStatus = 0; // collision occured
@@ -222,6 +222,7 @@ int main()
       arm2.x = arm1.x + arm_2_length*cos((theta_1+theta_2)*PI/180.0);
       arm2.y = arm1.y + arm_2_length*sin((theta_1+theta_2)*PI/180.0);
 
+      // cout << "Arm 2 (x,y) = " << arm2.x << ", " << arm2.y << endl;
       // check collision of arm 2
       if(map_2.lineCollision(arm1, arm2)){
         collisionStatus = 0; // collision occured
@@ -241,18 +242,107 @@ int main()
   cSpaceFile.close();
 
 
+  // //////////////////////////////////////////////////////////////////////////////
+  // //////////////////////////////////////////////////////////////////////////////
+  //
+  // // Exercise 3 part c
+  //
+  // //////////////////////////////////////////////////////////////////////////////
+  // // read points in from obstacles.txt and make a vector of obstacles
+  // // This will be a vector of polygons, which in turn a vector of positions
+  // std::vector<polygon> obstacles_3;
+  //
+  // string line; // whole line of text file
+  // int count = 1; // helps get pairs of 2
+  // vertex ver;
+  // // read in obstacle file
+  // ifstream myfile("obstacle_Ex_3_c.txt");
+  // while ( getline (myfile,line) ){
+  //
+  //   std::istringstream ss( line);
+  //
+  //   // create new polygon object to be added
+  //   polygon poly;
+  //
+  //   while (ss){
+  //     string s; // string that is seperated by comma
+  //
+  //     if (!getline( ss, s, ',' )) break; // if no more commas, break while
+  //
+  //     if(count ==1){ // get x value
+  //       // add x value of point
+  //       ver.x = std::stof(s);
+  //       count = 2;
+  //     }
+  //     else{ // get y value
+  //       // add y value of point
+  //       ver.y = std::stof(s);
+  //       // add vertex to the polygon in obstacles
+  //       poly.vertices.push_back(ver);
+  //       count =1;
+  //     }
+  //
+  //
+  //   }
+  //
+  //   obstacles_3.push_back(poly); // push the polygon into obstacles vector
+  // }
+  // myfile.close();
+  //
+  //
+  // // make a map object
+  // map map_3(xLim, yLim, start, goal, obstacles_3);
+  //
+  // double delta_theta = 1; // number of points from 0-360 degrees
+  //
+  // double arm_1_length = 1;
+  // double arm_2_length = 1;
+  //
+  // double theta_1 = 0;
+  // double theta_2 = 0;
+  //
+  // vertex origin, arm1,arm2;
+  // origin.x = 0; origin.y =0;
+  //
+  // int collisionStatus = 1; // collision 1 means no collision occured
+  // // map_1.printPolygons();
+  //
+  //
+  // ofstream cSpaceFile ("cSpace_Ex_3_c.txt");
+  //
+  // while(theta_1 <= 360){
+  //
+  //   while(theta_2 <= 360){
+  //
+  //     // make function to get position of points
+  //     arm1.x = arm_1_length*cos(theta_1*PI/180.0);
+  //     arm1.y = arm_1_length*sin(theta_1*PI/180.0);
+  //     // cout << "Arm 1 (x,y) = " << arm1.x << ", " << arm1.y << endl;
+  //     // check collision of arm 1
+  //     if(map_3.lineCollision(origin, arm1)){
+  //       collisionStatus = 0; // collision occured
+  //     }
+  //
+  //     arm2.x = arm1.x + arm_2_length*cos((theta_1+theta_2)*PI/180.0);
+  //     arm2.y = arm1.y + arm_2_length*sin((theta_1+theta_2)*PI/180.0);
+  //
+  //     // cout << "Arm 2 (x,y) = " << arm2.x << ", " << arm2.y << endl;
+  //     // check collision of arm 2
+  //     if(map_3.lineCollision(arm1, arm2)){
+  //       collisionStatus = 0; // collision occured
+  //     }
+  //
+  //     cSpaceFile << theta_1 << "," << theta_2 << "," << collisionStatus << endl;
+  //
+  //     collisionStatus  = 1 ;
+  //     theta_2 = theta_2 + delta_theta;
+  //
+  //   }
+  //
+  //   theta_2 = 0;
+  //   theta_1 = theta_1 + delta_theta;
+  // }
+  //
+  // cSpaceFile.close();
 
-	// // save C-Space obstacles to txt file
-	// ofstream cSpaceFile ("cSpace_Ex_3_a.txt");
-	// int cnt = 0;
-	// for(const auto& poly_it: map_1.c_space_obs){
-  //
-	// 	for(const auto& vert_it: poly_it.vertices){
-	// 		cSpaceFile << vert_it.x << "," << vert_it.y << ", " << angle[cnt] << endl;
-	// 	}
-  //
-	// 	cnt++;
-	// }
-  //
-	// cSpaceFile.close();
 }
